ea904e8380062ee9e347c19f74c44d757dc8724b
Will Wayman <will.jdw@gmail.com>
2018-12-12 15:51:44 +0000

    refactored test framework

--- TestFramework/TestFramework.js
+++ TestFramework/TestFramework.js
@@ -2,31 +2,47 @@
 
   function Test() {
 
-    return {
-      compare: function (actual, expectation) {
-        if (actual === expectation) {
-          console.log('Test Passed');
-        } else {
-          console.log('Test Failed');
-        }
-      },
-      compareArray: function (actual, expectation) {
-        if (actual.length !== expectation.length) {
-          console.log('Test Failed');
-        } else {
-          var differencesCounter = actual.length;
-          for (var i = 0; i < actual.length; i++) {
-            if (actual[i] === expectation[i]) {
-              differencesCounter --;
-            }
-          }
-          if (differencesCounter > 0) {
-            console.log('Test Failed');
-          } else {
-            console.log('Test Passed');
-          }
+    var testResult = "Test not completed.";
+
+    var compare = function (actual, expectation) {
+      if (actual === expectation) {
+        testResult = "Test Passed.";
+      } else {
+        testResult = "Test Failed.";
+      }
+      console.log(testResult);
+    }
+
+    var compareArray = function (actual, expectation) {
+      if (actual.length !== expectation.length) {
+        testResult = "Test Failed."
+      } else {
+        compareArrayItems(actual, expectation);
+      }
+      console.log(testResult);
+    }
+
+    var compareArrayItems = function (actual, expectation) {
+      var differencesCounter = actual.length;
+      for (var i = 0; i < actual.length; i++) {
+        if (actual[i] === expectation[i]) {
+          differencesCounter --;
         }
       }
+      checkArraysResult(differencesCounter);
+    }
+
+    var checkArraysResult = function (num) {
+      if (num > 0) {
+        testResult = "Test Failed.";
+      } else {
+        testResult = "Test Passed.";
+      }
+    }
+
+    return {
+      compare: compare,
+      compareArray: compareArray
     }
   }
 
