bef3dee867b1595ebb33a6b00897534d7ef2acb1
Will Wayman <will.jdw@gmail.com>
2018-12-12 11:25:08 +0000

    wrapped note list model in module pattern

--- note-list-model.js
+++ note-list-model.js
@@ -1,16 +1,22 @@
-function NoteList() {
-  this.notes = [];
-}
+(function (exports) {
 
-NoteList.prototype.addNote = function (string) {
-  var note = new Note(string);
-  this.notes.push(note);
-};
+  function NoteList() {
+    var notes = [];
 
-NoteList.prototype.viewNotes = function () {
-  var notesArray = []
-  for (var i = 0; i < this.notes.length; i++) {
-    notesArray.push(this.notes[i].returnText());
-  };
-  return notesArray;
-};
+    return {
+      addNote: function (string) {
+        var note = new Note(string);
+        notes.push(note);
+      },
+      viewNotes: function () {
+        var notesArray = []
+        for (var i = 0; i < notes.length; i++) {
+          notesArray.push(notes[i].returnText());
+        }
+        return notesArray;
+      }
+   }
+  }
+
+  exports.NoteList = NoteList;
+})(this);
